96be1a00f9f6eb9b3d7a9662e4e10f3f3e8d8c0d
Muthu <s.muthukhumar@gmail.com>
2019-07-21 13:52:08 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'cart_page.dart';
+import 'home_bloc.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<CartBloc>(
+        builder: (context) => CartBloc(),
+        child: MaterialApp(
+          title: 'Flutter Shopping Cart Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: MyHomePage(title: 'Gift Shop'),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var bloc = Provider.of<CartBloc>(context);
+    int totalCount = 0;
+    if (bloc.cart.length > 0) {
+      totalCount = bloc.cart.values.reduce((a, b) => a + b);
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          new Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: new Container(
+                height: 150.0,
+                width: 30.0,
+                child: new GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CartPage(),
+                      ),
+                    );
+                  },
+                  child: new Stack(
+                    children: <Widget>[
+                      new IconButton(
+                        icon: new Icon(
+                          Icons.shopping_cart,
+                          color: Colors.white,
+                        ),
+                        onPressed: null,
+                      ),
+                      new Positioned(
+                          child: new Stack(
+                        children: <Widget>[
+                          new Icon(Icons.brightness_1,
+                              size: 20.0, color: Colors.red[700]),
+                          new Positioned(
+                              top: 3.0,
+                              right: 7,
+                              child: new Center(
+                                child: new Text(
+                                  '$totalCount',
+                                  style: new TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              )),
+                        ],
+                      )),
+                    ],
+                  ),
+                )),
+          )
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(6, (index) {
+          return GestureDetector(
+              onTap: () {
+                bloc.addToCart(index);
+              },
+              child: Container(
+                height: 200,
+                width: 200,
+                padding: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/ic_${index + 1}.jpeg"),
+                    fit: BoxFit.fitWidth,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ));
+        }),
+      ),
+    );
+  }
+}
